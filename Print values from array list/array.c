a518a2a7f341d59b24d61462698a43f2ed9fda0e
TH_Toufique <138722742+TH-Toufique@users.noreply.github.com>
2024-03-01 19:27:25 +0600

    Print values from array list

---
+++ Print values from array list/array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int array [] = {1,2,3,4,5};
+    printf("%d\n", array[0]);
+    printf("%d\n", array[1]);
+    printf("%d\n", array[2]);
+    printf("%d\n", array[3]);
+    printf("%d\n", array[4]);
+}
