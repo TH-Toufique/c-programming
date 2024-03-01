96934af11cca8cc090cca91abfa1f1196dbd77a4
TH_Toufique <138722742+TH-Toufique@users.noreply.github.com>
2024-03-01 14:02:48 +0600

    Print upside down pyramid of * using for loop

---
+++ Print upside down pyramid of */udp.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int row;
+    int col;
+    printf("Enter number of rows you want to print: ");
+    scanf("%d",&row);
+    for (i = 1; i <= row; row--)
+    {
+        for(col = 1; col <= row; col++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
